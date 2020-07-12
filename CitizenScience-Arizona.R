68a69906a91f8f486a8897628db95feee6577660
klprudic <klprudic@gmail.com>
2020-07-12 14:00:25 -0700

    Add percent change analysis
    - Change column renaming to tidyverse
    - Add percent change plots
    - Add percent change t-tests

--- CitizenScience-Arizona.R
+++ CitizenScience-Arizona.R
@@ -24,9 +24,11 @@ arizona_counts <- arizona_spring %>%
   summarize(num_observations = n(), 
             num_users = length(unique(user_id)))
 
-# Rename columns
-colnames(arizona_counts)[1:2] <- c("county", "year")
-  
+# Rename columns from place_county_name to county and year(observed_on) to year
+arizona_counts <- arizona_counts %>% 
+  rename(county = place_county_name, 
+         year = "year(observed_on)")
+
 # Plot number of observations 
 obs_plot <- ggplot(data = arizona_counts, mapping = aes(x = year, 
                                                         y = num_observations,
@@ -42,3 +44,65 @@ users_plot <- ggplot(data = arizona_counts, mapping = aes (x = year,
                                                            color = county)) +
   geom_line()
 users_plot
+
+# Calculate percent change in observations 2018-2019 and 2019-2020 
+# by spring (Mar-May) and county
+# ((observations in year t/ observations in year t-1) -1) * 100
+
+arizona_obs_change <- arizona_counts %>% 
+  mutate(prev_obs = lag(num_observations)) %>%
+  filter(!is.na(prev_obs)) %>%
+  mutate(per_change_obs = (((num_observations/prev_obs)-1) * 100)) %>%
+  select(-prev_obs)
+
+# Plot percent change in observations
+change_obs_plot <- ggplot(data = arizona_obs_change, 
+                          mapping = aes(x = year, 
+                                        y = per_change_obs,
+                                        group = county, 
+                                        color = county)) +
+  geom_line()
+change_obs_plot
+
+
+# Calculate percent change in users 2018-2019 and 2019-2020 
+# by spring (Mar-May) and county
+
+arizona_users_change <- arizona_counts %>% 
+  mutate(prev_users = lag(num_users)) %>%
+  filter(!is.na(prev_users)) %>%
+  mutate(per_change_users = (((num_users/prev_users)-1) * 100)) %>%
+  select(-prev_users)
+
+# Plot percent change in users
+
+change_users_plot <- ggplot(data = arizona_users_change, 
+                          mapping = aes(x = year, 
+                                        y = per_change_users,
+                                        group = county, 
+                                        color = county)) +
+  geom_line()
+change_users_plot
+
+# Create dataframe for observations t test
+arizona_obs_ttest <- arizona_obs_change %>% 
+  select(county, year, per_change_obs) %>%
+  pivot_wider(names_from = year, values_from = per_change_obs)
+
+# t test on change in growth in observations
+
+obs_t_test <- t.test(x = arizona_obs_ttest$`2019`, 
+                     y = arizona_obs_ttest$`2020`, 
+                     paired = TRUE)
+
+# Create dataframe for users t test
+arizona_users_ttest <- arizona_users_change %>% 
+  select(county, year, per_change_users) %>%
+  pivot_wider(names_from = year, values_from = per_change_users)
+
+# t test on change in growth in observations
+
+users_t_test <- t.test(x = arizona_users_ttest$`2019`, 
+                     y = arizona_users_ttest$`2020`, 
+                     paired = TRUE)
+
