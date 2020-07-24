dd4f89f42ac3df1ec157b90041dec51920a303b3
tcrimmins <theresa@usanpn.org>
2020-07-23 18:41:37 -0700

    pushing updated script (urban intersection)

--- COVID_analysis.R
+++ COVID_analysis.R
@@ -86,3 +86,30 @@ hist(bigdf$sumCount,
 #write out data files as .csv
 write.csv(bigdf, file = 'bigdf.csv')
 write.csv(summarizedCell, file = 'summarizedCell.csv')
+
+##Urban/Rural designation for sites
+library(sp)
+library(rgdal)
+library(raster)
+
+#read in urban census boundaries layer as spatial data frame
+shape <- readOGR(dsn = ".", layer = "tl_2017_us_uac10")
+
+#plot urban boundaries 
+plot(shape)
+
+#convert site visits df to spatial data frame
+spdf<-SpatialPointsDataFrame(cbind(site_visits_AM$Longitude,site_visits_AM$Latitude),site_visits_AM)
+
+## CRS arguments: NA
+crs(shape) <- CRS("+proj=longlat +datum=WGS84")
+crs(spdf) <- CRS("+proj=longlat +datum=WGS84")
+
+#intersect points with urban boundaries
+o <- over(spdf, shape)
+spdf@data = cbind(spdf@data, shape[o,])
+site_visits_AM-urban <-cbind.data.frame(site_visits_AM,o)
+
+
+
+
